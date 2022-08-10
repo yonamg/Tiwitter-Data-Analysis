394ee335fbf9f4f5187634af198c3bb6a095ebff
yonamg <softeng2006@gmail.com>
2022-08-10 03:54:12 +0300

    convert column to datetime

--- fix_clean_tweets_dataframe.py
+++ fix_clean_tweets_dataframe.py
@@ -34,10 +34,9 @@ class Clean_Tweets:
         """
         convert column to datetime
         """
-        ----
-        
-        ----
-        
+        self.df['created_at'] = pd.to_datetime(self.df['created_at'], errors='coerce')
+        return self.df
+
         df = df[df['created_at'] >= '2020-12-31' ]
         
         return df
